6d8fc73dd67fdc6ac0a442e21b921e8ef9262750
PeteGabriel <pedrogabriel@protonmail.ch>
2018-03-04 19:04:32 +0000

    Add packages for Mongo, Passport, Jest and supertest.

---
+++ tests/academy/get_academy.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Request the current academy', () => {
+    test('It should respond with a valid representation of an academy resource', async () => {
+        const response = await request(app).get('/academy');
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe('application/json');
+    });
+})
